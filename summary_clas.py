a3443f0b08afbf2f7bedf9bd65872fb822eab1fe
olumidejacob <olumideaji101@gmail.com>
2024-12-04 13:12:29 -0800

    This is a program to print a list backwards (from the last item to the first item)

---
+++ summary_clas.py
@@ -0,0 +1,25 @@
+# def calculate_velocity(u,a,t):
+#     u=float(input("enter your initial velocity:"))
+#     a=float(input("enter your acceleration:"))
+#     t=float(input("enter your time:"))
+#     velocity = u+a,t
+#     print(velocity)
+# (a,t)
+
+# while True:
+#     print('hello world')
+
+
+# x=0
+
+# while x <10:
+#     print(x)
+#     x=x+1
+
+for i in range(10):
+    print(i)
+
+for i in range (3,500,3):
+    print(i)
+
+
